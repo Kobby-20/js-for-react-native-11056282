11a15031ad1aaa6df702ef4bc0c08e9aa2ccb1f1
Kobby-20 <151926930+Kobby-20@users.noreply.github.com>
2024-05-18 23:15:16 +0200

    userInfo.js

---
+++ userInfo.js
@@ -0,0 +1,8 @@
+function createUserProfiles(names, modifiedNames){
+  return names.map((name, i) => ({
+    id: i + 1,
+    originalName: name,
+    modifiedName: modifiedNames[i]
+  }));
+}
+export {createUserProfiles};
